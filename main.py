c4293788dc54a1e6e0caf3d4a485a29c713b58e1
Taseen-Zuhayr <taseen.zuhayr@gmail.com>
2024-09-21 22:39:57 +0600

    Divide

---
+++ main.py
@@ -0,0 +1,7 @@
+me = int(input("Enter numerator here : "))
+you = int(input("Enter denominator here : "))
+
+if me%you == 0:
+    print(me," is divided by ",you)
+else:
+    print(me," is NOT divided by ",you)
